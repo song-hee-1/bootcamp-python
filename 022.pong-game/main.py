37b1692d1b0d99ff08b23a7c332d6f8e58e05b29
한송희 <98_han@naver.com>
2022-04-06 12:21:35 +0900

    Complete pong-game

--- 022.pong-game/main.py
+++ 022.pong-game/main.py
@@ -1,5 +1,9 @@
 from turtle import Screen
 from paddle import Paddle
+from ball import Ball
+from scoreboard import Scoreboard
+import time
+
 
 screen = Screen()
 screen.bgcolor("black")
@@ -9,6 +13,8 @@ screen.tracer(0)
 
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
+ball = Ball()
+scoreboard = Scoreboard()
 
 screen.onkey(r_paddle.go_up, "Up")
 screen.onkey(r_paddle.go_down, "Down")
@@ -17,6 +23,22 @@ screen.onkey(l_paddle.go_down, "s")
 
 game_is_on = True
 while game_is_on:
+    time.sleep(ball.move_speed)
     screen.update()
+    ball.move()
+
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+        scoreboard.l_point()
+
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+        scoreboard.r_point()
+
+    if ball.xcor() > 380:
+        ball.reset_position()
+
+    if ball.ycor() < -380:
+        ball.reset_position()
 
 screen.exitonclick()
