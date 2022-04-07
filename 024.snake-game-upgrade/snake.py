0b8391297a185d821979359c78e7cff1651c2e94
한송희 <98_han@naver.com>
2022-04-07 15:55:39 +0900

    Upgrade 020-021.snake-game adding a high score to a file

---
+++ 024.snake-game-upgrade/snake.py
@@ -0,0 +1,60 @@
+from turtle import Turtle
+
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
+
+class Snake:
+
+    def __init__(self):
+        self.segements = []
+        self.create_snake()
+        self.head = self.segements[0]
+
+    def create_snake(self):
+        for position in STARTING_POSITIONS:
+            self.add_segment(position)
+
+    def add_segment(self, position):
+        new_segement = Turtle(shape="square")
+        new_segement.color("white")
+        new_segement.penup()
+        new_segement.goto(position)
+        self.segements.append(new_segement)
+
+    def reset(self):
+        for seg in self.segements:
+            seg.goto(1000, 1000)
+        self.segements.clear()
+        self.create_snake()
+        self.head = self.segements[0]
+
+    def extend(self):
+        self.add_segment(self.segements[-1].position())
+
+    def move(self):
+        for seg_num in range(len(self.segements) - 1, 0, -1):
+            new_x = self.segements[seg_num - 1].xcor()
+            new_y = self.segements[seg_num - 1].ycor()
+            self.segements[seg_num].goto(new_x, new_y)
+        self.head.forward(MOVE_DISTANCE)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
