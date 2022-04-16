a37242209e1fed96808c39eb17eec5b9352a3073
한송희 <98_han@naver.com>
2022-04-16 16:47:25 +0900

    Exception Handling

--- 026.nato-alphabet-project/main.py
+++ 026.nato-alphabet-project/main.py
@@ -21,7 +21,7 @@ import pandas
 # Keyword Method with iterrows()
 # {new_key:new_value for (index, row) in df.iterrows()}
 
-#TODO 1. Create a dictionary in this format:
+# TODO 1. Create a dictionary in this format:
 # {"A": "Alfa", "B": "Bravo"}
 
 
@@ -29,8 +29,19 @@ data = pandas.read_csv("nato_phonetic_alphabet.csv")
 
 phonetic_dic = {row.letter: row.code for (index, row) in data.iterrows()}
 
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 
-word = input("Enter a word: ").upper()
-output_list = [phonetic_dic[letter] for letter in word]
-print(output_list)
+# TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+
+
+def generate_phonetic():
+    word = input("Enter a word: ").upper()
+    try:
+        output_list = [phonetic_dic[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please")
+        generate_phonetic()
+    else:
+        print(output_list)
+
+
+generate_phonetic()
